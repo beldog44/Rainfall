f22191f3d00a57f836da9ad8a296358eee4f131a
田中太郎 <t-kamano@softbank126017073004.bbtec.net>
2017-04-25 23:55:12 +0900

    特定の日付を変数に代入してスクレイピングを可能にした

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -11,3 +11,4 @@ import scrapy
 class WebItem(scrapy.Item):
     # define the fields for your item here like:
      name = scrapy.Field()
+     time = scrapy.Field()
--- tutorial/tutorial/spiders/webspider.py
+++ tutorial/tutorial/spiders/webspider.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from datetime import date, datetime
 from tutorial.items import WebItem
 from scrapy.linkextractors import LinkExtractor
 
 class WebSpiderSpider(scrapy.Spider):
     name = "webspiderSpider"
     allowed_domains = ["http://www.data.jma.go.jp/"]
-    start_urls = ['http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=62&block_no=47772&year=2017&month=3&day=1&view=a4']
+    d1 = date(2016, 6, 19)                #調査年月日
+    dd1 = (d1.day)                        #調査日付のみ
+    #d = datetime.date(20:16, 6, 27)
+    d2 = date(2017, 3, 31)                #調査最終年月日
+    #print (d1)
+    #td = datetime.timedelta(days=1)  #for文で回るようにする
+    start_urls = ['http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=62&block_no=47772&year=2016&month=06&day={0}&view=a4'.format(dd1)]
 
     def parse(self, response):
        article = WebItem()
-       article['name'] = response.css("td.data_0_0").extract_first()
+       article['time']  = response.xpath('//td/text()').extract()
+       # article['time']  = response.css("td.white-space:nowrap").extract_first()
+       #article['name'] = response.css("td.data_0_0").extract_first()
        yield article
