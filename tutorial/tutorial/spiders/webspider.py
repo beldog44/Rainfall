7ee28d2c02a94a74d5908e4e720c4b935f5effa6
田中太郎 <t-kamano@softbank126017073004.bbtec.net>
2017-04-26 01:28:11 +0900

    while文で複数日程回るようにした

--- tutorial/tutorial/spiders/webspider.py
+++ tutorial/tutorial/spiders/webspider.py
@@ -1,24 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-from datetime import date, datetime
+import datetime
+from datetime import date
 from tutorial.items import WebItem
 from scrapy.linkextractors import LinkExtractor
 
 class WebSpiderSpider(scrapy.Spider):
     name = "webspiderSpider"
     allowed_domains = ["http://www.data.jma.go.jp/"]
-    d1 = date(2016, 6, 19)                #調査年月日
+    d1 = date(2017, 3, 29)                #調査年月日
     dd1 = (d1.day)                        #調査日付のみ
     #d = datetime.date(20:16, 6, 27)
     d2 = date(2017, 3, 31)                #調査最終年月日
     #print (d1)
     #td = datetime.timedelta(days=1)  #for文で回るようにする
-    start_urls = ['http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=62&block_no=47772&year=2016&month=06&day={0}&view=a4'.format(dd1)]
+    while d1 <= d2:
+        print ('start')
+        #dd1 = (d1.day)                #繰り返す度に日付を更新するため
+        #dd1 = (d1.day)
+        start_urls = ['http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=62&block_no=47772&year=2017&month=03&day={0}&view=a4'.format(dd1)]
+        d1 = d1 + datetime.timedelta(days = 1)
+        dd1 = (d1.day)
+   # else:
+    #    print ('end')
 
-    def parse(self, response):
-       article = WebItem()
-       article['time']  = response.xpath('//td/text()').extract()
+        def parse(self, response):
+          article = WebItem()
+          article['time']  = response.xpath('//td/text()').extract()
        # article['time']  = response.css("td.white-space:nowrap").extract_first()
        #article['name'] = response.css("td.data_0_0").extract_first()
-       yield article
+          yield article
+    
+    else:
+        print ('end')
